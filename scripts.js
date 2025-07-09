e250b7f8ed35fa786f8bd24b3f6b9e1c0644e503
Davi Lorena <davilorena2016@gmail.com>
2025-07-09 14:11:09 -0300

    adiciona codigo inicial de javascript

---
+++ scripts.js
@@ -0,0 +1,18 @@
+const apiKeyInput = document.getElementById('apiKey')
+
+const gameSelect = document.getElementById('gameSelect')
+
+const questionInput = document.getElementById('questionInput')
+
+const askButton = document.getElementById('askButton')
+
+const form = document.querySelector('form')
+
+const aiResponse = document.getElementById('aiResponse')
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault()
+
+
+  
+})
